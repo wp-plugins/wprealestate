1b0cdaaf175ce5e44e955fa55b9782379f53eb47
hozyali <hozyali@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-06 09:05:43 +0000

    releasing 3.2
    git-svn-id: https://plugins.svn.wordpress.org/wprealestate/tags/3.2@870475 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ et_re_home.php
@@ -0,0 +1,35 @@
+<h1>WP Real Estate - Home</h1>
+<p><strong>NOTE</strong>: <em>If you do all the settings correctly and still see 404 page not found errors, please go to your <a href="options-permalink.php">wp-admin &gt; settings &gt; permalinks</a> and simply press the save button. It will reset your permalinks and make the plugins links active. Pressing the save button does not need to change the actual permalinks configuration.</em></p>
+<p>A quick  video tutorial is located here <a href="http://www.youtube.com/watch?v=PDkLs5lj9G4" target="_blank">http://www.youtube.com/watch?v=PDkLs5lj9G4</a></p>
+<p>Thank you for choosing WP Real Estate plugin. I hope you will like the plugin and enjoy using it. If you have any support or sales related queries, please feel free to send an email to <a href="mailto:sales@etechysolutions.com.my">sales@etechysolutions.com.my.</a></p>
+<h3>What's new in version 3.0</h3>
+<ul style="list-style: disc inside none;">
+  <li>Unlimited property photos can be added</li>
+  <li>Enable/Disable social sharing</li>
+  <li>Custom property types have been added. You can add or manage your own <a href="admin.php?page=WPRealEstatePropertyType">custom property types from here</a>.</li>
+  <li>Advanced property search has been added. You can customize the form by selecting what fields you want to use for <a href="admin.php?page=WPRealEstateSettings">advanced search from here</a>.</li>
+  <li>Also make sure you have selected the correct property listing page.</li>
+</ul>
+<h3>Stay Updated and get more customized features directly to your inbox</h3>
+<p>
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="http://etechy101.us2.list-manage1.com/subscribe/post?u=b8210f8523d1e31f37518d48e&amp;id=155e3516fe" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+<strong>I promise, you won't get a single spam</strong>. Leave your email below
+<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_b8210f8523d1e31f37518d48e_155e3516fe" value=""></div>
+	<div class="clear"><input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+</p>
+<hr />
+<p>Would you like us to feature your website in our show case? Please send us your name, email and website address to <a href="mailto:sales@etechysolutions.com.my">sales@etechysolutions.com.my</a> and we will be glad to feature your website using WP Real Estate on our website's show case.</p>
